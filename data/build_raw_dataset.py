fe72d97e5be54b01d9b0fd672b29dbf193224966
anfalsiddiqui <anfalsiddiqui@salesforce.com>
2022-02-03 13:23:39 -0800

    intial commit

---
+++ data/build_raw_dataset.py
@@ -0,0 +1,81 @@
+import tweet_retrieval
+import pandas as pd
+import numpy as np
+import time
+
+TWITTER_API_RATE_LIMIT = 30000
+
+EMOJIS = "👌,💔,💗,😂,😆,😉,😙,😚,😜,😨,😩,😲,🙀"
+EMOJI_MAP_FILE = "/Users/anfalsiddiqui/development/cs329s/Project/data/5822100/emoji_map_1791.csv"
+RAW_DATA_FILE = "/Users/anfalsiddiqui/development/cs329s/Project/data/5822100/full_train_plaintext.txt"
+PROCESSED_OUTPUT_FILE = "/Users/anfalsiddiqui/development/cs329s/Project/data/processed/full_train.csv"
+
+def get_emoji_ids():
+    df = pd.read_csv(EMOJI_MAP_FILE)
+    ids = []
+    for emoji in EMOJIS.split(","):
+        ids.append(df[df['Unnamed: 0'] == emoji].index[0])
+    return ids
+
+def create_conditions_from_ids(ids, raw_dataset_df):
+    conditions = []
+    for id in ids:
+        conditions.append(raw_dataset_df['annotations'].str.contains(str(id)))
+    return conditions
+
+def convert_twitter_api_response_to_processed_rows(twitter_api_resp, df):
+    rows = []
+    data = twitter_api_resp['data']
+    for record in data:
+        id, text = record["id"], record["text"]
+        annotations = df[df["id"] == id]["annotations"].iloc[0]
+        rows.append({id: id, text: text, annotations: annotations})
+    return rows
+
+def create_and_save_csv_from_rows(rows):
+    df = pd.DataFrame(rows)
+    df.to_csv("")
+
+
+def filter_raw_dataset(raw_dataset_df, df_conditions):
+    return raw_dataset_df.loc[np.logical_or.reduce(df_conditions) & ~raw_dataset_df['id'].str.endswith("_q")]
+
+
+if __name__ == "__main__":
+    raw_dataset_df = pd.read_csv(RAW_DATA_FILE, sep="\t")
+    df_conditions = create_conditions_from_ids(get_emoji_ids(), raw_dataset_df)
+    raw_dataset_filtered_df = filter_raw_dataset(raw_dataset_df, df_conditions)
+    processed_rows = []
+
+    for i in range(len(raw_dataset_filtered_df) // TWITTER_API_RATE_LIMIT + 1):
+        request_limit_subset = raw_dataset_filtered_df[TWITTER_API_RATE_LIMIT * i:TWITTER_API_RATE_LIMIT * (i + 1)]
+        for j in range(len(request_limit_subset) // 100):
+            request_subset = request_limit_subset[100*j:100*(j+1)]
+            cur_batch_tweet_ids = request_subset["id"].tolist()
+            twitter_api_resp = tweet_retrieval.retrieve_tweets(cur_batch_tweet_ids)
+            processed_rows += convert_twitter_api_response_to_processed_rows(twitter_api_resp, request_subset)
+        time.sleep(900)
+
+    create_and_save_csv_from_rows(processed_rows)
+
+
+
+
+
+
+
+
+
+# convert the list of emojis into their IDs
+# create a list of conditions based on all these ids
+# filter down the dataset
+# create a new pandas dataframe that we will save to
+# for every 90,000 rows:
+#   for every 100 rows:
+#       map id to list of emojis in it
+#       send request to twitter
+#       for every member of data:
+#           get tweet, id from data
+#           get list of emojis by id
+#           add row to dataframe that is [id, tweet, emoji]
+    # sleep for 15 minutes
---
+++ data/tweet_retrieval.py
@@ -0,0 +1,61 @@
+import requests
+import os
+import json
+
+# To set your enviornment variables in your terminal run the following line:
+# export 'BEARER_TOKEN'='<your_bearer_token>'
+bearer_token = "AAAAAAAAAAAAAAAAAAAAAPJCYwEAAAAAcGowBFzpiulxvo76tf15K%2FUkvLU%3DfaIu7F0ZUwlKiZOeGoAVbQjUHHIs8V4AUUqQvAWCXT4cScgq6R"
+
+
+def create_url(tweet_ids):
+    assert len(tweet_ids) <= 100
+    tweet_fields = "tweet.fields=lang,author_id"
+    # Tweet fields are adjustable.
+    # Options include:
+    # attachments, author_id, context_annotations,
+    # conversation_id, created_at, entities, geo, id,
+    # in_reply_to_user_id, lang, non_public_metrics, organic_metrics,
+    # possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets,
+    # source, text, and withheld
+    ids = "ids=" + ",".join(tweet_ids)
+    # You can adjust ids to include a single Tweets.
+    # Or you can add to up to 100 comma-separated IDs
+    url = "https://api.twitter.com/2/tweets?{}&{}".format(ids, tweet_fields)
+    return url
+
+
+def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
+
+    r.headers["Authorization"] = f"Bearer {bearer_token}"
+    r.headers["User-Agent"] = "v2TweetLookupPython"
+    return r
+
+
+def connect_to_endpoint(url):
+    response = requests.request("GET", url, auth=bearer_oauth)
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(
+            "Request returned an error: {} {}".format(
+                response.status_code, response.text
+            )
+        )
+    return response.json()
+
+def retrieve_tweets(tweet_ids):
+    url = create_url(tweet_ids)
+    json_response = connect_to_endpoint(url)
+    return json_response
+
+def main():
+    # url = create_url([])
+    # json_response = connect_to_endpoint(url)
+    # print(json.dumps(json_response, indent=4, sort_keys=True))
+    retrieve_tweets(["743525594323783680", "742723328864063488", "746328513016303616"])
+
+
+if __name__ == "__main__":
+    main()
