ef49feb9d8747c87cf3f370d894bb57fed7262d9
JP Reilly <jpreilly@stanford.edu>
2022-02-05 13:52:23 -0800

    sample streamlit

--- streamlit-app/interface.py
+++ streamlit-app/interface.py
@@ -10,13 +10,22 @@ class EmotifyMachine:
     def choose_random(self):
         return np.random.choice(self.emojis)
 
+    def probability_dist(self):
+        return np.random.uniform(low=0.0, high=1.0, size=(len(self.emojis),))
 
-em = EmotifyMachine()
 
-title = st.title('Emotify')
-subheader = st.subheader(
-    'Bring your tweets and messages to life with the help of ML generated emojis!')
-text = st.text_area('Emotify machine', value='', max_chars=280,
-                    placeholder='Paste your tweet (280 characters max) here')
-if st.button('Click to Emotify!'):
-    st.write('Your chosen emoji: ', em.choose_random())
+def run_streamlit():
+    em = EmotifyMachine()
+
+    title = st.title('Emotify')
+    subheader = st.subheader(
+        'Bring your tweets and messages to life with the help of ML generated emojis!')
+    text = st.text_area('Emotify machine', value='', max_chars=280,
+                        placeholder='Paste your tweet (280 characters max) here')
+    if st.button('Click to Emotify!'):
+        st.write('Your chosen emoji: ', em.choose_random())
+        st.bar_chart(data=em.probability_dist())
+
+
+if __name__ == "__main__":
+    run_streamlit()
