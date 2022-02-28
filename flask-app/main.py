141e9e634f9e7942fc6a8e66434a8d6fb461850c
JP Reilly <jpreilly@stanford.edu>
2022-02-27 19:55:09 -0800

    cors

--- flask-app/main.py
+++ flask-app/main.py
@@ -1,8 +1,11 @@
 import json
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from transformers import pipeline, AutoTokenizer
 import os
+
 app = Flask(__name__)
+CORS(app)
 
 tokenizer = AutoTokenizer.from_pretrained(
     "vinai/bertweet-base", normalization=True)
